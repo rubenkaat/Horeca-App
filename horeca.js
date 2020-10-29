b3b3fd44f1bbaa62ca306917796ca26dca00ca50
Ruben Kaat <99034608@mydavinci.nl>
2020-10-29 14:58:46 +0100

    functie bestellen gemaakt
    bier, wijn, fris bestellen gemaakt

--- horeca.js
+++ horeca.js
@@ -1,7 +1,32 @@
-const frisPrice = 1;
-const wijnPrice = 3;
-const bierPrice = 2;
+const frisPrice = 1.00;
+const wijnPrice = 3.00;
+const bierPrice = 2.00;
+
+var frisTotaal = 0;
+var wijnTotaal = 0;
+var bierTotaal = 0;
 
 
 alert("Welkom bij het cafe!")
-prompt("Welke bestelling wilt u toevoegen?")
+
+function order(){
+	var bestelling = prompt('Wat wilt u bestellen?');
+		if (bestelling == 'fris'){
+			var frisAantal = prompt('Hoeveel fris wilt u toevoegen aan uw bestelling?');
+			alert('U heeft ' + frisAantal + 'fris toegevoegd aan uw bestelling.');
+			 frisTotaal = frisTotaal + frisAantal;
+			 order();
+		}else if (bestelling == 'wijn'){
+			var wijnAantal = prompt('Hoeveel wijn wilt u toevoegen aan uw bestelling?');
+			alert('U heeft ' + wijnAantal + 'wijn toegevoegd aan uw bestelling.');
+			wijnTotaal = wijnTotaal + wijnAantal;
+			order();
+		}else if( bestelling == 'bier'){
+			var bierAantal = prompt('Hoeveel bier wilt u toevoegen aan uw bestelling?');
+			alert('U heeft ' + bierAantal + 'bier toegevoegd aan uw bestelling.');
+			bierTotaal = bierTotaal + bierAantal;
+			order();
+		}else if(bestelling == 'stop'){
+			stopOrder();// functie prijs berekenen maken
+		}
+}
